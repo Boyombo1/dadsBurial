3335b6ce7b0e5a455f6d84b922cd692207be26d1
Boyombo1 <bayuba6526@gmail.com>
2023-06-20 07:30:24 +0100

    search input and permission added

--- finance/urls.py
+++ finance/urls.py
@@ -5,5 +5,7 @@ urlpatterns = [
     path('addincome/', views.income_add, name='addincome'),
     path('addexpense/', views.expence_add, name='addexpense'),
     path('allexpense/', views.allexpence, name='allexpense'),
-    path('allincome/', views.allincome, name='allincome')
+    path('allincome/', views.allincome, name='allincome'),
+    path('edit-income/<int:id>/', views.edit_income, name='edit-income'),
+    path('delete-income/<int:id>/', views.delete_income, name='delete-income')
 ]
--- finance/views.py
+++ finance/views.py
@@ -6,13 +6,14 @@ from .models import Income, Expence
 from.forms import addIncomeForm, addExpenceForm
 from .utils import incomeLeft
 from django.db.models import Sum
+from django.contrib.auth.models import User
 #from djmoney.money import Money
 from decimal import Decimal
 
 # Create your views here.
 
 def home(request):
-    return render(request, "home.html")
+    return render(request, "login.html")
 
 
 @login_required(login_url='login')
@@ -29,6 +30,25 @@ def income_add(request):
     return render(request, 'finance/income_add.html', {'form':form})
 
 @login_required(login_url='login')
+def edit_income(request, id):
+    income = get_object_or_404(Income, id=id)
+    #income = Income.objects.get(id=id)
+    if request.method == "POST":
+        form = addIncomeForm(request.POST, instance=income)
+        if form.is_valid():
+            form.save()
+            #Income.get_total_amount
+        return redirect('allincome')
+    else:
+        form = addIncomeForm()
+    return render(request, 'finance/income_add.html', {'form':form, "income":income})
+
+def delete_income(request, id):
+    income = get_object_or_404(Income, pk=id)
+    income.delete()
+    return redirect('allincome')
+
+@login_required(login_url='login')
 @permission_required("finance.can_add_expense", raise_exception=True)
 def expence_add(request):
     if request.method == "POST":
@@ -42,16 +62,22 @@ def expence_add(request):
         form = addExpenceForm()
     return render(request, 'finance/expence_add.html', {'form':form})
 
-
+@login_required(login_url='login')
 def allincome(request):
-    income = Income.objects.all()
+    search_query = ""
+
+    if request.GET.get("search_query"):
+        search_query = request.GET.get("search_query")
+
+    income_s = Income.objects.filter(owner__username__icontains=search_query)
+    #income = Income.objects.all()
     income_sum = Income.objects.aggregate(total=Sum('amount')).get('total', Decimal('0.00'))
     expense = Expence.objects.aggregate(total=Sum('amount')).get('total', Decimal('0.00'))
     balance = incomeLeft()
-    context = {"income":income, "income_sum":income_sum, "balance":balance, "expense":expense}
+    context = {"income_s": income_s, "income_sum":income_sum, "balance":balance, "expense":expense}
     return render(request, 'finance/income.html', context)
 
-
+@login_required(login_url='login')
 def allexpence(request):
     expense = Expence.objects.all()
     income = Income.objects.aggregate(total=Sum('amount')).get('total', Decimal('0.00'))
@@ -73,18 +99,21 @@ def loginuser(request):
         try: 
             user = User.objects.get(username=username)
 
-        except:
+        except User.DoesNotExist:
             messages.error(request, "username does not exist")
+            return redirect('login')
         
         user = authenticate(request, username=username, password = password)
 
         if user is not None:
             login(request, user)
-            return redirect('home')
+            return redirect('allexpense')
         else:
             messages.error(request, "username or password is incorrect")
+            return redirect('login')
+
     return render(request, 'login.html')
 
 def logoutuser(request):
     logout(request)
-    return redirect('home')       
+    return redirect('login')       
